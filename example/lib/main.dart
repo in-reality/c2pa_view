6bbca8922ac75b5b274a3cbde3d6aa5be39ae618
Jeppe <northguard@tutanota.com>
2025-04-18 21:03:42 +0200

    Example can run in web.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:c2pa_view/c2pa_view.dart';
 
+// Decent introduction: https://medium.com/flutter-community/how-to-create-publish-and-manage-flutter-packages-b4f2cd2c6b90
+
+// Run with:
+// flutter_rust_bridge_codegen build-web
+// flutter run --web-header=Cross-Origin-Opener-Policy=same-origin --web-header=Cross-Origin-Embedder-Policy=require-corp
+
 Future<void> main() async {
   await RustLib.init();
   runApp(const MyApp());
